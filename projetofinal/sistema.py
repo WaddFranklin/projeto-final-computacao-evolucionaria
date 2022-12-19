d8a97323af943d372a444ad64a11c0ec7dfcea0e
Willieny Barbosa <wbm@ic.ufal.br>
2022-12-19 19:58:13 -0300

    Tela inicial do sistema

---
+++ projetofinal/sistema.py
@@ -0,0 +1,16 @@
+from lib.interface import *
+from time import sleep
+
+while True:
+    resposta = menu(['Recomendação de compra',
+                    'Listar estoque', 'Sair do sistema'])
+    if resposta == 1:
+        cabeçalho('Opção 1')
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema...')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(1)
